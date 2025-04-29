03bf70e51375019d0acdab3b46013a253f8f7f4c
larrybwosi <larrybwosi@gmail.com>
2025-04-29 22:21:51 +0300

    comment out bun

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,19 @@ export default async ({ req, res, log }:any) => {
 
 // Uncomment the following lines to run the server
 // import {Bun} from 'bun'
-const server = Bun.serve({
-  port: 3000,
-  routes: {
-    '/home': () => new Response('Hello World!'),
-    '/main.js': () => Bun.file('./src/main.js'),
-    '/pdf': async (req: Request) => {
-      console.log('Received request for PDF generation:', await req.json());
-      // const pdfBuffer = await generateAndSaveReceiptPdf(mockSaleData);
-      // console.log('PDF Buffer:', pdfBuffer);
-      return new Response('pdfBuffer');
-    },
-  },
-});
+// const server = Bun.serve({
+//   port: 3000,
+//   routes: {
+//     '/home': () => new Response('Hello World!'),
+//     '/main.js': () => Bun.file('./src/main.js'),
+//     '/pdf': async (req: Request) => {
+//       console.log('Received request for PDF generation:', await req.json());
+//       // const pdfBuffer = await generateAndSaveReceiptPdf(mockSaleData);
+//       // console.log('PDF Buffer:', pdfBuffer);
+//       return new Response('pdfBuffer');
+//     },
+//   },
+// });
 
 
 
